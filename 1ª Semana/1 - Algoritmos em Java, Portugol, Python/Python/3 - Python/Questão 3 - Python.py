a9da522343a9fac92e2b74a0e886ef16c5d1b790
Pedro Florencio <pedroflorencio@alu.ufc.br>
2022-02-01 17:29:13 -0300

    impedindo entrada de valores negativos

--- 1ª Semana/1 - Algoritmos em Java, Portugol, Python/Python/3 - Python/Questão 3 - Python.py
+++ 1ª Semana/1 - Algoritmos em Java, Portugol, Python/Python/3 - Python/Questão 3 - Python.py
@@ -1,36 +1,29 @@
 # Questão 3 - O usuário vai informar um valor e o programa vai retornar se este numero é primo
 
-numeroInteiro = False
+numeroInteiroPositivo= False
 
 # Verifica se é um número válido, se não pede novamente
-while (numeroInteiro == False):
+while (numeroInteiroPositivo == False):
     
     valor = input('Digite um valor válido (número natural) ')
 
     try:
         valor = int(valor)
-        numeroInteiro = True
+        if valor <= 0:
+            numeroInteiroPositivo = False
+        else:
+            numeroInteiroPositivo = True
 
     except ValueError:
-         numeroInteiro = False
+         numeroInteiroPositivo = False
 
 # Contagem do número de divisíveis
 contador = 0
 
 print('\n{} é divisível pelos seguintes algarismos:'.format(valor),end='')
 
-# Se o número for negativo, o loop de divisões iniciará do valor de entrada até -1
-if valor < 0:
-    inicioLoop = valor
-    fimLoop = 0
-
-# Se o número for positivo, o loop de divisões iniciará de 1 até o valor de entrada
-else:
-    inicioLoop = 1
-    fimLoop = valor+1
-
 # Divisão do valor de entrada por todos algarismos anteriores a ele
-for c in range(inicioLoop, fimLoop, 1):
+for c in range(1, valor + 1):
 
     if valor%c == 0:
         print('[{}] '.format(c),end='')
@@ -42,6 +35,9 @@ for c in range(inicioLoop, fimLoop, 1):
 # Condição que imprime na tela se o número é primo ou não
 if contador > 2:
     print('\n\nComo o valor é divisível por {} algarismos, ele não é primo'.format(contador))
+
+elif contador == 1:
+    print('\n\nComo o valor é divisível por apenas {} algarismo, ele não é primo'.format(contador))
 
 else:
     print('\n\nComo o valor é divisível por apenas {} algarismos, ele é primo'.format(contador))   
