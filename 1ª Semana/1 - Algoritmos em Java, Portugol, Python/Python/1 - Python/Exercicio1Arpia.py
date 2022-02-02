b4ef41c5a7b0613f374e1921a247077ec2670c08
yanstivaletti <yanstivalettiesouza15@gmail.com>
2022-02-02 15:52:54 -0300

    mudancas Python

--- 1ª Semana/1 - Algoritmos em Java, Portugol, Python/Python/1 - Python/Exercicio1Arpia.py
+++ 1ª Semana/1 - Algoritmos em Java, Portugol, Python/Python/1 - Python/Exercicio1Arpia.py
@@ -1,16 +1,19 @@
 numero = int(input("digite o primeiro numero:"))
 maior = numero
 menor = numero
+numero = -1
 
 for i in range(4):
-    
-    numero = int(input("digite o próximo numero: "))
+    while (numero < 0):
+        numero = int(input("digite o próximo numero: "))
     
     if(numero > maior):
         maior = numero
         
     if(numero < menor):
         menor = numero
+    
+    numero = -1
            
 print("O maior numero é: ")
 print(maior)
